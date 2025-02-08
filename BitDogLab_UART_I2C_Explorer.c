f9aa39ffea6584f079beefdb73abe91bc55f8d8e
hsantosdias@gmail.com <hsantosdias@gmail.com>
2025-02-08 13:07:04 -0300

    Add display animation logic in main function

--- BitDogLab_UART_I2C_Explorer.c
+++ BitDogLab_UART_I2C_Explorer.c
@@ -107,6 +107,22 @@ int main() {
     ssd1306_t ssd;
     init_display(&ssd);
 
+    bool cor = true;
+    while (true)
+    {
+      cor = !cor;
+      // Atualiza o conteúdo do display com animações
+      ssd1306_fill(&ssd, !cor); // Limpa o display
+      ssd1306_rect(&ssd, 3, 3, 122, 58, cor, !cor); // Desenha um retângulo
+      ssd1306_draw_string(&ssd, "Cepedi   TIC37", 8, 10); // Desenha uma string
+      ssd1306_draw_string(&ssd, "EMBARCATECH", 20, 30); // Desenha uma string
+      ssd1306_draw_string(&ssd, "PROF WILTON", 15, 48); // Desenha uma string      
+      ssd1306_send_data(&ssd); // Atualiza o display
+  
+      sleep_ms(1000);
+    }
+
+    /* 
     while (1)
     {
         // Verifica se a flag está ativa
@@ -131,6 +147,7 @@ int main() {
         // Pequeno atraso para evitar sobrecarga no loop principal
         sleep_ms(10);
     }
+    */
 }
 
 
