d5b5b170c9d996b498f63162956c7fd2b15f44e3
hsantosdias@gmail.com <hsantosdias@gmail.com>
2025-02-08 12:25:33 -0300

    add test for UART initialization and GPIO configuration; implement LED blinking and button interrupt handling

--- BitDogLab_UART_I2C_Explorer.c
+++ BitDogLab_UART_I2C_Explorer.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
@@ -42,6 +43,66 @@ void gpio_irq_handler(uint gpio, uint32_t events)
     }
 }
 
+
+
+int main()
+{
+    // Inicializa a biblioteca padrão
+    stdio_init_all();
+/*     Inicializa a UART com o padrão: ( 115200, 8N1)
+*    115200: A taxa de transmissão de dados.
+*    8: O número de bits de dados por caractere (neste caso, 8 bits).
+*    N: Não há bit de paridade (No parity).
+*    1: O número de bits de parada (neste caso, 1 bit).
+*    Low bit first: A ordem dos bits é do bit menos significativo para o mais significativo.
+*/
+    uart_init(UART_ID, BAUD_RATE);
+
+    // Configura os pinos GPIO para a UART
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART); // Configura o pino 0 para TX
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART); // Configura o pino 1 para RX
+
+    // Configura o pino do LED
+    gpio_init(LED_PIN_R);
+    gpio_set_dir(LED_PIN_R, GPIO_OUT);
+
+    // Configura o pino do botão com resistor pull-up
+    gpio_init(BUTTON_PIN_A);
+    gpio_set_dir(BUTTON_PIN_A, GPIO_IN);
+    gpio_pull_up(BUTTON_PIN_A);
+
+    // Configuração da interrupção com callback
+    gpio_set_irq_enabled_with_callback(BUTTON_PIN_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+
+    while (1)
+    {
+        // Verifica se a flag está ativa
+        if (flag_var)
+        {
+            // Cria a mensagem com o valor atual do contador
+            char message[50];
+            sprintf(message, "Contagem: %d\n", counter);
+
+            // Envia a mensagem pela UART
+            uart_puts(UART_ID, message);
+            //            printf("Mensagem enviada: %s", message); // Exibe a mensagem no serial monitor
+
+            // Pisca o LED
+            gpio_put(LED_PIN_R, 1);
+            sleep_ms(50);
+            gpio_put(LED_PIN_R, 0);
+            sleep_ms(50);
+            flag_var = 0; // Limpa a flag de interrupção
+        }
+
+        // Pequeno atraso para evitar sobrecarga no loop principal
+        sleep_ms(10);
+    }
+
+    return 0;
+}
+
+/*
 int main()
 {
   stdio_init_all(); // Inicializa a UART para debug
@@ -95,3 +156,5 @@ int main()
     sleep_ms(1000);
   }
 }
+
+*/
