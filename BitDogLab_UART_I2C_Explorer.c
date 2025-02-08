5cd0e72a5d700bbe47302669f95f78a6ae7e7187
hsantosdias@gmail.com <hsantosdias@gmail.com>
2025-02-08 11:57:16 -0300

    add UART initialization and GPIO interrupt handling; configure button with debouncing

--- BitDogLab_UART_I2C_Explorer.c
+++ BitDogLab_UART_I2C_Explorer.c
@@ -3,18 +3,70 @@
 #include "hardware/i2c.h"
 #include "inc/ssd1306.h"
 #include "inc/font.h"
+#include "hardware/irq.h"
+// Definições do display SSD1306 128x64 I2C OLED 
 #define I2C_PORT i2c1
 #define I2C_SDA 14
 #define I2C_SCL 15
 #define endereco 0x3C
+//Definições para uart 
+#define UART_ID uart0    // Seleciona a UART0
+#define BAUD_RATE 115200 // Define a taxa de transmissão
+#define UART_TX_PIN 0    // Pino GPIO usado para TX
+#define UART_RX_PIN 1    // Pino GPIO usado para RX
+#define LED_PIN_R 13       // Pino GPIO usado para o LED Vermelho
+#define LED_PIN_B 12       // Pino GPIO usado para o LED Azul
+#define LED_PIN_G 11       // Pino GPIO usado para o LED Verde
+#define BUTTON_PIN_A 5    // Pino GPIO usado para o botão A
+#define BUTTON_PIN_B 6    // Pino GPIO usado para o botão B
 
+// Variáveis globais
+static volatile uint16_t counter = 0;   // Variável inteira para contagem
+static volatile uint32_t last_time = 0; // Armazena o tempo do último evento (em microssegundos)
+static volatile bool flag_var = 0;      // Variável de flag para debouncing
 
+// Prototipação da função de interrupção
+static void gpio_irq_handler(uint gpio, uint32_t events);
+
+// Função de interrupção com debouncing
+void gpio_irq_handler(uint gpio, uint32_t events)
+{
+    // Obtém o tempo atual em microssegundos
+    uint32_t current_time = to_us_since_boot(get_absolute_time());
+    // Verifica se passou tempo suficiente desde o último evento
+    if (current_time - last_time > 200000) // 50 ms de debouncing
+    {
+        last_time = current_time; // Atualiza o tempo do último evento
+        counter++;                // Incrementa o contador
+        flag_var = 1;
+    }
+}
 
 int main()
 {
-  // I2C Initialisation. Using it at 400Khz.
+  stdio_init_all(); // Inicializa a UART para debug
+  
   i2c_init(I2C_PORT, 400 * 1000);
 
+  gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART); // Define a função do pino GPIO para UART TX
+  gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART); // Define a função do pino GPIO para UART RX
+  uart_init(UART_ID, BAUD_RATE); // Inicializa a UART
+
+    // Configura o pino do LED
+    gpio_init(LED_PIN_R);
+    gpio_set_dir(LED_PIN_R, GPIO_OUT);
+
+
+    // Configura o pino do botão com resistor pull-up
+    gpio_init(BUTTON_PIN_A);
+    gpio_set_dir(BUTTON_PIN_A, GPIO_IN);
+    gpio_pull_up(BUTTON_PIN_A);
+
+
+  // Configuração da interrupção com callback
+  gpio_set_irq_enabled_with_callback(BUTTON_PIN_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+
+  // I2C Initialisation. Using it at 400Khz.
   gpio_set_function(I2C_SDA, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
   gpio_set_function(I2C_SCL, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
   gpio_pull_up(I2C_SDA); // Pull up the data line
